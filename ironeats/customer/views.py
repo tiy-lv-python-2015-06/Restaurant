65eab477a133d7439931a3dce400a29623b6a24e
Eric Turner <EricTurnerNV@gmail.com>
2015-08-14 15:43:33 -0700

    added home view

--- ironeats/customer/views.py
+++ ironeats/customer/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from restaurant.models import Restaurant
+
+
+class home(ListView):
+    model = Restaurant
+    template_name = "home.html"
+    queryset = Restaurant.objects.all()
+    context_object_name = 'movies'
+    paginate_by = 20
