bdf0db297392ed81329ce15d7da7eb3195ccc1e2
Eric Turner <EricTurnerNV@gmail.com>
2015-08-14 14:37:36 -0700

    fixed admin

--- ironeats/restaurant/admin.py
+++ ironeats/restaurant/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from customer.models import Customer, Order
+from restaurant.models import Restaurant, FoodItem
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'address', 'city',
+                    'state', 'zip', 'phone',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('restaurant', 'customer', 'menu', 'timestamp')
+
+
+@admin.register(FoodItem)
+class FoodItemAdmin(admin.ModelAdmin):
+    list_display = ('restaurant', 'name', 'price', 'description')
