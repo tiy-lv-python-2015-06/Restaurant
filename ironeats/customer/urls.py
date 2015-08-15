b5ca5c3071b6afa5dc28385597ce9ce3c9a5ff99
Aaron Pentz <apentz49@gmail.com>
2015-08-15 08:29:05 -0700

    login for both restaurant and customer now working

--- ironeats/customer/urls.py
+++ ironeats/customer/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'', include('django.contrib.auth.urls'), name='login'),
+    url(r'/login/', auth_views.login,
+        {'login.html': 'templates/registration/login.html'}),
 ]
--- ironeats/restaurant/urls.py
+++ ironeats/restaurant/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib.auth.decorators import login_required
 from restaurant.views import RestaurantProfile, OrderList
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
@@ -8,5 +9,8 @@ urlpatterns = [
         RestaurantProfile.as_view(), name='restaurant_profile'),
     url(r'^order_list/', OrderList.as_view(), name='order_list'),
         url(r'^', include('django.contrib.auth.urls'), name='login'),
+        url(r'', include('django.contrib.auth.urls'), name='login'),
+    url(r'/login/', auth_views.login,
+        {'login.html': 'templates/registration/login.html'}),
 
 ]
