5d9567cc62e3a73eb4504d1a2a7c26a98dfec434
Traci Armstrong <traciarms@gmail.com>
2015-08-14 19:21:37 -0700

    restaurant profile and order list pages - first draft

--- ironeats/restaurant/urls.py
+++ ironeats/restaurant/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib.auth.decorators import login_required
-from restaurant.views import RestaurantProfile
+from restaurant.views import RestaurantProfile, OrderList
 
 urlpatterns = [
 
-    url(r'^restaurant_profile/', RestaurantProfile.as_view(),
-        name='restaurant_profile'),
+    url(r'^restaurant_profile/(?P<restaurant_id>[0-9]+)/',
+        RestaurantProfile.as_view(), name='restaurant_profile'),
+
+    url(r'^order_list/', OrderList.as_view(), name='order_list'),
 
 ]
