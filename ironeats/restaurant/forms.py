9ed71ce54adab048e8821732fec1280520aa6b0b
Aaron Pentz <apentz49@gmail.com>
2015-08-14 20:22:26 -0700

    updated forms

--- ironeats/restaurant/forms.py
+++ ironeats/restaurant/forms.py
@@ -1,11 +1,13 @@
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 from restaurant.models import Restaurant, FoodItem
 
 
-class RestaurantForm(ModelForm):
+class UserForm(ModelForm):
     class Meta:
-        model = Restaurant
-        fields = ()
+        model = User, Restaurant
+        fields = ('Business Name', 'address', 'city', 'state', 'zipcode',
+                  'phone number', 'username', 'password')
 
 
 class FoodForm(ModelForm):
